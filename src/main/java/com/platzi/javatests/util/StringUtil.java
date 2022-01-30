f5631c5b2caa0e2084d79322d4c897dc2bdfd600
Alberto Juan Gabino <intrunet@gmaial.com>
2022-01-30 13:42:25 -0600

    Comprobando repeticiones correctas y validaciones con AssertEqual

--- src/main/java/com/platzi/javatests/util/StringUtil.java
+++ src/main/java/com/platzi/javatests/util/StringUtil.java
@@ -2,4 +2,13 @@ package com.platzi.javatests.util;
 
 public class StringUtil {
 
+    public static String repeat(String str, int times) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < times; i++) {
+            result.append(str);
+        }
+
+        return result.toString();
+    }
 }
--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -2,6 +2,20 @@ package com.platzi.javatests.util;
 
 public class StringUtilTest {
 
-    }
+    public static void main(String[] args) {
 
+        String result = StringUtil.repeat("Hola", 3);
+        if(result.equals("HolaHolaHola")){
+            System.out.println("Ok");
+        }else{
+            System.out.println("ERROR");
+        }
+
+        String result2 = StringUtil.repeat("Hola", 1);
+        if(result2.equals("Hola")){
+            System.out.println("Ok");
+        }else{
+            System.out.println("ERROR");
+        }
+    }
 }
