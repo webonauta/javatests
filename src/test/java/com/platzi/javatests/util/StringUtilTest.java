ec3d5972634c5a39dc28796ad1a4b26b046f1911
Alberto Juan Gabino <intrunet@gmaial.com>
2022-01-30 22:09:01 -0600

    Refactorizando asserts quitando variable result

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -4,11 +4,9 @@ public class StringUtilTest {
 
     public static void main(String[] args) {
 
-        String result = StringUtil.repeat("Hola", 3);
-        assertEquals(result, "HolaHolaHola");
+        assertEquals(StringUtil.repeat("Hola", 3), "HolaHolaHola");
 
-        String result2 = StringUtil.repeat("Hola", 1);
-        assertEquals(result2, "Hola");
+        assertEquals(StringUtil.repeat("Hola", 1), "Hola");
     }
 
     private static void assertEquals(String actual, String expected) {
