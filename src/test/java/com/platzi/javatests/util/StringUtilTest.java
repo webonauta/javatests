157e0cfbe07061632529625949f2e0fd1669b975
Alberto Juan Gabino <intrunet@gmaial.com>
2022-01-30 22:06:44 -0600

    Mejorando implementacion de errores con excepciones

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -5,17 +5,16 @@ public class StringUtilTest {
     public static void main(String[] args) {
 
         String result = StringUtil.repeat("Hola", 3);
-        if(result.equals("HolaHolaHola")){
-            System.out.println("Ok");
-        }else{
-            System.out.println("ERROR");
-        }
+        assertEquals(result, "HolaHolaHola");
 
         String result2 = StringUtil.repeat("Hola", 1);
-        if(result2.equals("Hola")){
-            System.out.println("Ok");
-        }else{
-            System.out.println("ERROR");
+        assertEquals(result2, "Hola");
+    }
+
+    private static void assertEquals(String actual, String expected) {
+
+        if(!actual.equals(expected)) {
+            throw new RuntimeException(actual + "is not equal to expected" + expected);
         }
     }
 }
